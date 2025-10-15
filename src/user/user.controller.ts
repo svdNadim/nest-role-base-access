e7d74742c19d1a8847b898a1b8b5709015037d80
svdNadim <nadimcottonsdt@gmail.com>
2025-10-15 09:55:51 +0600

    implemented and test post api

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Role } from '@prisma/client';
+import { UserService } from './user.service';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  // Controller methods would go here
+  constructor(private readonly userService: UserService) {}
+
+  @Post('create-user')
+  async createUser(
+    @Body()
+    body: {
+      email: string;
+      name: string;
+      password: string;
+      role?: Role;
+    },
+  ) {
+    return this.userService.createuser(body);
+  }
+
+  @Get()
+  async getAllUsers() {
+    return this.userService.getAllUsers();
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,52 @@
 import { Injectable } from '@nestjs/common';
+import { Role } from '@prisma/client';
+import bcrypt from 'bcrypt';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  // Create User with hashed password
+  async createuser(data: {
+    email: string;
+    name: string;
+    password: string;
+    role?: Role;
+  }) {
+    // Check the email is alreday exits or not
+    const isUserExist = await this.prisma.user.findUnique({
+      where: { email: data.email },
+    });
+    if (isUserExist) {
+      return {
+        message: 'User with this email already exists',
+        status: 400,
+      };
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(data.password, 12);
+
+    // Create User and auth together
+    const user = await this.prisma.user.create({
+      data: {
+        email: data.email,
+        name: data.name,
+        role: data.role || Role.BUYER,
+        auth: {
+          create: {
+            password: hashedPassword,
+          },
+        },
+      },
+    });
+
+    return user;
+  }
+
+  // Get all users
+  async getAllUsers() {
+    return this.prisma.user.findMany();
+  }
+}
